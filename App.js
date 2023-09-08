a5b59f8a7b8a025d9570fe29cbf9c554325b9a5e
irahultiwari <rahultiwari07133@gmail.com>
2023-09-08 13:10:36 +0530

    change

--- App.js
+++ App.js
@@ -1,19 +1,56 @@
 import React from 'react';
-import {Button, Text, View} from 'react-native';
+import {Button, Text, TextInput, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
 const App = () => {
+  const btnaction =()=>{
+    console.warn('btn pressd ')
+  }
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} />
+      <Stack.Navigator 
+      screenOptions={{
+        headerStyle:{
+          backgroundColor:'skyblue'
+          
+        },
+        headerTintColor:'black',
+        headerTitleStyle:{
+          fontSize:30
+        }
+      }}
+      
+      >
+        <Stack.Screen name="Login" component={Login}
+        options={{
+          headerTitle:()=><Button onPress={btnaction} title='left' />,
+          headerRight:()=><Header />,
+
+          title:'user login',
+          headerStyle:{
+            backgroundColor:'skyblue',
+
+          },
+          headerTintColor:'white',
+          headerTitleStyle:{
+            fontSize:30
+          }
+        }}
+        
+        />
         <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
+
+const Header=()=>{
+  return(
+    <TextInput placeholder='SEARCH'></TextInput>
+  )
+}
 
 const Home = () => {
   return (
